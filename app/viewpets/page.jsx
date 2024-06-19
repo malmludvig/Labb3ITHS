6d7452e28019da12a056f2e8f44cc9843c3a0a04
Ludvig <malmludvig@gmail.com>
2024-06-20 00:30:21 +0200

    Changed localstroage fuinctionality to client side

--- app/viewpets/page.jsx
+++ app/viewpets/page.jsx
@@ -1,54 +1,44 @@
 "use client"
 
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 import Link from "next/link"
-import CreateTemplatePets from '../../components/CreateTemplatePets';
-import TemplateLink from '../../components/TemplateLink';
 
-export default function HomeView () {
+export default function HomeView() {
+    const [storedDatas, setStoredDatas] = useState([]);
 
-    //Om det är första gången besökaren använder dogbook så skapar jag en tom array som sparas i localStorage.
-    if (localStorage.getItem('datas') === null) 
-    {
-        let emptyArray = []
-        localStorage["datas"] = JSON.stringify(emptyArray);
-    } 
+    useEffect(() => {
+        // Om det är första gången besökaren använder dogbook så skapar jag en tom array som sparas i localStorage.
+        if (localStorage.getItem('datas') === null) {
+            let emptyArray = []
+            localStorage.setItem("datas", JSON.stringify(emptyArray));
+        }
 
-    var stored_datas = JSON.parse(localStorage["datas"]);
+        var storedDatas = JSON.parse(localStorage.getItem("datas"));
+        setStoredDatas(storedDatas);
+    }, []);
 
     const deleteDog = (id) => {
+        const updatedDatas = storedDatas.map(item => {
+            const updatedFriendList = item.friendList.filter(x => x !== id);
+            return { ...item, friendList: updatedFriendList };
+        }).filter(item => item.dogId !== id);
 
-        for (let i = 0; i < stored_datas.length; i++) {
-        
-            const temp1 = stored_datas[i].friendList
-            const temp2 = temp1.filter(x => x !== id);
-
-            stored_datas[i].friendList = temp2
-          }
-
-        const removeIndex = stored_datas.findIndex( item => item.dogId === id );
-        stored_datas.splice(removeIndex, 1);
-
-        localStorage["datas"] = JSON.stringify(stored_datas);
-        window.location.reload();
-
+        localStorage.setItem("datas", JSON.stringify(updatedDatas));
+        setStoredDatas(updatedDatas);
     }
 
     return (
         <div>
-            
-        <h1>Dogs:</h1>
-            {stored_datas.map((item, index) => (
+            <h1>Dogs:</h1>
+            {storedDatas.map((item, index) => (
                 <div key={index}>
-                    { <Link href={"/pet/" + item.name.toLowerCase()}>{item.name}</Link>}
-
-                    <a onClick={() => deleteDog(item.dogId, item.name)} className="deleteLink"> [delete]</a>
+                    <Link href={"/pet/" + item.name.toLowerCase()}>{item.name}</Link>
+                    <a onClick={() => deleteDog(item.dogId)} className="deleteLink"> [delete]</a>
                     <img className="profileImg" src={item.img} alt="" />
-                    <br/>
+                    <br />
                 </div>
-                ))}
-                <br/>
-                
+            ))}
+            <br />
         </div>
     )
 }
